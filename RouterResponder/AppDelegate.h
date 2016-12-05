6400f5b2fa1117c922ddb7e2ec2b5f153f9e5722
hefasong <794486315@qq.com>
2016-12-05 15:16:25 +0800

    Initial Commit

---
+++ RouterResponder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RouterResponder
+//
+//  Created by 何发松 on 2016/12/5.
+//  Copyright © 2016年 HeRay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RouterResponder/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RouterResponder
+//
+//  Created by 何发松 on 2016/12/5.
+//  Copyright © 2016年 HeRay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
